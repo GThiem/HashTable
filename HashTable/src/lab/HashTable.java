258385a6282f976d57ab73d0f25f42319582b999
GThiem <53498973+GThiem@users.noreply.github.com>
2020-06-18 19:55:10 +0200

    Add files via upload

--- HashTable/src/lab/HashTable.java
+++ HashTable/src/lab/HashTable.java
@@ -18,6 +18,16 @@ public class HashTable {
 	protected int hash_b;
 	protected LinkedList[] entryLists;
 	
+	//DELETEpowefjqwfoqjefopqwempdoppppppppppppppppppppppppppppppppppppwwwwwwwwwwwwwwwwwwww
+	//dqwiodfqowfpqwonqponvnnnnnnnnnnnnnnnnnnn
+	public static void main(String[] args) {
+		HashTable a = new HashTable(4);
+		System.out.println(a.capacity);
+		System.out.println(a.hash_a);
+		System.out.println(a.hash_b);
+		
+	}
+	
 	 /**
      * The constructor
      * 
@@ -25,19 +35,70 @@ public class HashTable {
      *            represents the initial size of the Hash Table.
      * 
      * The Hash-Table itself should be implemented in the array entryLists. When the
-     * load factor exceeds 75%, the capacity of the Hash-Table should be
+     * load factor exceeds 70%, the capacity of the Hash-Table should be
      * increased as described in the method rehash below.
      */
 	public HashTable(int initialCapacity) {
 		// TODO
+		int inCap = initialCapacity;
+        while(!isPrime(inCap)) {
+            inCap++;
+        }
+        
+        capacity = inCap;
+        
+        Random random = new Random();
+        hash_a = random.nextInt(capacity-1)+1;
+        hash_b = random.nextInt(capacity);
+        
+        entryLists = new LinkedList[capacity];
 	}
+	
+	public boolean isPrime(int num) {
+        boolean isPrime = true;
+        
+        if(num == 1)
+        	return false;
+        
+        for(int i = 2; i <= num/2; i++) {
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
 	
 	/**
 	 * Search for an TableEntry with the given key. If no such TableEntry is found, return null.
 	 */
 	public TableEntry find(String key) {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
+		ListNode node = null;
+		
+		for(LinkedList list : entryLists) {
+			if(list != null) {
+				ListNode e = list.head();
+				while(e != list.nil() && e.entry().getKey() != key) {
+					e = e.next();
+				}
+				
+				if(e != list.nil()) {
+					node = e;
+					break;
+				} else {
+					node = null;
+				}
+			}
+		}
+		
+		if(node != null) {
+			return node.entry();
+		}
+		
+		return null;
+		
 	}
 	
 	/**
@@ -47,7 +108,31 @@ public class HashTable {
 	 */
 	public void insert(TableEntry entry) {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
+		String key = entry.getKey();
+		TableEntry found = find(key);
+		
+		if(found == null) {
+			for(int i = 0; i < entryLists.length; i++) {
+				if(hash(key) == i) {
+					if(entryLists[i] != null) {
+						entryLists[i].insertBefore(entry, entryLists[i].head());
+					}
+					else {
+						entryLists[i] = new LinkedList();
+						entryLists[i].append(entry);
+					}
+				}
+			}
+		} else {
+			found.setData(entry.getData());
+		}
+		
+		double max = capacity * 0.7;
+		
+		if(size() >= max) {
+			rehash();
+		}
+		
 	}
 	
 	/**
@@ -56,23 +141,55 @@ public class HashTable {
 	 */
 	public TableEntry delete(String key) {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+			ListNode node = null;
+			
+			for(LinkedList list : entryLists) {
+				if(list != null) {
+					//FALSCH
+					ListNode e = list.head();
+					while(e != list.nil()){
+						if(e.entry().getKey().equals(key)) {
+							node = e;
+			                list.delete(e);
+							return node.entry();
+							
+						}
+						e = e.next();
+					}
+				}
+			}
+			
+			return null;
+		}
 	
 	/**
 	 * The hash function used in the hash table.
 	 */
 	public int hash(String x) {
-		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+        int result = 0;
+
+        for (int i = 0; i < x.length(); i++) {
+            result += (Math.pow(3, i) + i) % Math.pow(2, 16) * (int) (x.charAt(i)) % Math.pow(2, 16);
+        }
+        
+        result = (((hash_a * result % capacity) + (hash_b % capacity)) % capacity); 
+        return result;
+    }
 	
 	/**
 	 * Return the number of TableEntries in this hash table.
 	 */
 	public int size() {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
+		int sum = 0;
+		
+		for(LinkedList list : entryLists) {
+			if(list != null) {
+				sum += list.length();	
+			}
+		}
+		
+		return sum;
 	}
 	
 	/**
@@ -80,7 +197,34 @@ public class HashTable {
 	 */
 	private void rehash() {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
+		int num = capacity * 5;
+		while(!isPrime(num)) {
+            num++;
+        }
+		capacity = num;
+		
+		Random random = new Random();
+		hash_a = random.nextInt(capacity-1)+1;
+        hash_b = random.nextInt(capacity);
+        
+        HashTable newTable = new HashTable(capacity);
+        newTable.hash_a = hash_a;
+        newTable.hash_b = hash_b;
+        
+        for(LinkedList list : entryLists) {
+        	if(list != null) {
+				ListNode e = list.head();
+				while(e != list.nil()) {
+					ListNode temp = e.next();
+					newTable.insert(e.entry());
+					list.delete(e);
+					e = temp;
+				}
+        	}
+        }
+        
+        entryLists = newTable.entryLists;
+        
 	}
 	
 	/**
@@ -90,7 +234,16 @@ public class HashTable {
 	 */
 	public int quality() {
 		// TODO
-		throw new RuntimeException("Not implemented yet!");
+		int len = 0;
+		
+		for(LinkedList list : entryLists) {
+			if(list != null) {
+				if(list.length() > len)
+					len = list.length();
+			}
+		}
+		
+		return len;
 	}
 
 	public int getHash_a() {
--- HashTable/src/lab/LinkedList.java
+++ HashTable/src/lab/LinkedList.java
@@ -33,31 +33,82 @@ public class LinkedList {
 	 * Return the number of elements in the linked list.
 	 */
 	public int length() {
-		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+        // TODO
+        int counter = 0;
+        if(_head == _nil) {
+            return 0;
+        }
+        ListNode temp = _head;
+        while(temp.next() != _nil) { 
+            temp = temp.next();
+            counter++;
+        }
+        return counter+1;
+    }
 	
 	/**
 	 * Insert an entry into the linked list at the position before the given node.
 	 */
 	public void insertBefore(TableEntry entry, ListNode node) {
-		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+        // TODO
+        if(node == _head) {
+            ListNode newNode = new ListNode(entry,_nil,_head);
+            _head.setPrev(newNode);
+            _head = newNode;
+            return;
+        }
+        if(node == _nil) {
+            append(entry);
+            return;
+        }
+        ListNode temp = _head;
+        while(temp.next()!= _nil) {
+            if(temp == node) {
+                ListNode newNode = new ListNode(entry,temp.prev(),node);
+                temp.prev().setNext(newNode);
+                node.setPrev(newNode);
+                return;
+            } else
+                temp = temp.next();
+        }
+    }
 	
 	/**
 	 * Append an entry to the end of the list.
 	 */
 	public void append(TableEntry entry) {
-		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+        if(_head == _nil) {
+            _head = new ListNode(entry,_nil,_nil);
+            return;
+        }
+        ListNode temp = _head;
+        while(temp.next()!=_nil) {
+            temp = temp.next();
+        }
+        ListNode newNode = new ListNode(entry, temp,_nil);
+        temp.setNext(newNode);
+
+    }
 	
 	/**
 	 * Delete the given node from the linked list.
 	 */
 	public void delete(ListNode node) {
-		// TODO
-		throw new RuntimeException("Not implemented yet!");
-	}
+        ListNode temp = _head;
+        if(_head == _nil) {
+        	return;
+        }
+        if(node == _head) {
+        	_head.next().setPrev(_nil);
+        	_head = _head.next();
+        }
+        while(temp.next()!=_nil) {
+            if(temp == node) {
+                temp.prev().setNext(temp.next());
+                temp.next().setPrev(temp.prev());
+                return;
+            }else
+                temp = temp.next();
+        }
+    }
 }
